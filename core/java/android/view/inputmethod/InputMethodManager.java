27a1ffbb9e16e6bdc25531746a7f37d3983e16a4
Yohei Yukawa <yukawa@google.com>
2022-07-19 11:56:30 -0700

    [BACKPORT] Merge IInputMethodManager#get{,AwareLocked}InputMethodList
    This is a follow up CL to our previous CL [1], which introduced
    
      IInputMethodManager#getAwareLockedInputMethodList
    
    as a variant of
    
      IInputMethodManager#getInputMethodList.
    
    This CL mechanically merges those two IPC entries into one entry as
    specifying
    
      DirectBootAwareness.AUTO
    
    is something we can do in the IME client side, without changing any
    observable semantics.
    
     [1]: I54d5dbec7e76d6a68935007ed3af0641f717a7c5
          04a226334d4f2d6296d2837fa4ae165ef98e10e9
    
    Bug: 210083408
    Bug: 234882948
    Test: presubmit
    Change-Id: Ic02484fcbb9c1bbd8e964bb7a2f7acff904e2a33

--- core/java/android/view/inputmethod/InputMethodManager.java
+++ core/java/android/view/inputmethod/InputMethodManager.java
@@ -1433,7 +1433,7 @@ public final class InputMethodManager {
         // We intentionally do not use UserHandle.getCallingUserId() here because for system
         // services InputMethodManagerInternal.getInputMethodListAsUser() should be used
         // instead.
-        return mServiceInvoker.getInputMethodList(UserHandle.myUserId());
+        return mServiceInvoker.getInputMethodList(UserHandle.myUserId(), DirectBootAwareness.AUTO);
     }
 
     /**
@@ -1447,7 +1447,7 @@ public final class InputMethodManager {
     @RequiresPermission(INTERACT_ACROSS_USERS_FULL)
     @NonNull
     public List<InputMethodInfo> getInputMethodListAsUser(@UserIdInt int userId) {
-        return mServiceInvoker.getInputMethodList(userId);
+        return mServiceInvoker.getInputMethodList(userId, DirectBootAwareness.AUTO);
     }
 
     /**
@@ -1463,7 +1463,7 @@ public final class InputMethodManager {
     @NonNull
     public List<InputMethodInfo> getInputMethodListAsUser(@UserIdInt int userId,
             @DirectBootAwareness int directBootAwareness) {
-        return mServiceInvoker.getAwareLockedInputMethodList(userId, directBootAwareness);
+        return mServiceInvoker.getInputMethodList(userId, directBootAwareness);
     }
 
     /**
