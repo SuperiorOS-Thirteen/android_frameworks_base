e69634b2621e68b7ab45208dbc0c3cd03a5e41bf
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-16 06:00:12 +0000

    SystemUI: Make round QS toggleable [1/3]
    Change-Id: I86e3bc990b78e763bfc9fb4a7ed8ffeeff95cc3f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11882,6 +11882,12 @@ public final class Settings {
         public static final String QS_TILE_SHAPE = "qs_tile_shape";
 
         /**
+         * Quicksettings UI Style
+         * @hide
+         */
+        public static final String QS_STYLE_ROUND = "qs_style_round";
+
+        /**
          * Whether to disable qs on secure lockscreen.
          * Default 0
          * @hide
